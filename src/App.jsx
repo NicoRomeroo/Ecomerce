95de4150b21244206229d2a22125fad1ff167220
nicoromero <nicoriver122@gmail.com>
2024-04-16 17:00:35 -0300

    agregue el cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
-import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
-import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
-import NavBar from "./Components/NavBar/NavBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer"
+import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
+import NavBar from "./Components/NavBar/NavBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {CartProvider} from './context/CartContext'
+import { NotificationProvider } from './context/Notification'
+import Cart from './Components/Cart/Cart'
+import Checkout from './Components/Checkout/Checkout'
+
+
+
 function App() {
-
-
   return (
-    <>
+    <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route
-            path="/"
-            element={<ItemListContainer greetings={"Listado de Productos"} />}
-          />
-          <Route
-            path="/category/:categoryId"
-            element={<ItemListContainer greetings={"Listado de Productos filtrados"} />}
-          />
-          <Route path="/detail/:productId" element={<ItemDetailContainer />} />
-        </Routes>
+        <NotificationProvider>
+          <CartProvider>
+            <NavBar />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ItemListContainer greetings={"Listado de Productos"} />
+                }
+              />
+              <Route
+                path="/category/:categoryId"
+                element={
+                  <ItemListContainer
+                    greetings={"Listado de Productos filtrados"}
+                  />
+                }
+              />
+              <Route
+                path="/detail/:productId"
+                element={<ItemDetailContainer />}
+              />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+            </Routes>
+          </CartProvider>
+        </NotificationProvider>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
